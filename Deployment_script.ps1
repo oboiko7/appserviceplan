c76bd9d31799017304cd25409223cee18e117c98
oboiko7 <oleksandr_boiko7@epam.com>
2021-12-11 13:14:12 +0200

    app service plan update

---
+++ Deployment_script.ps1
@@ -0,0 +1,8 @@
+﻿Connect-AzAccount
+Get-AzSubscription
+Select-AzSubscription -SubscriptionName "Azure subscription 1"
+$mainTemplateUri="https://raw.githubusercontent.com/oboiko7/Homeworks/main/linked/virtual_network.json"
+New-AzResourceGroupDeployment `
+  -Name Homework `
+  -ResourceGroupName TestRG1 `
+  -TemplateUri $mainTemplateUri
