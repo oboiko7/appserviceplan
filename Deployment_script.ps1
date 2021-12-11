cc04462a06721da037de0f3f2c788326f7449e49
oboiko7 <oleksandr_boiko7@epam.com>
2021-12-11 13:24:47 +0200

    Update Deployment_script.ps1

--- Deployment_script.ps1
+++ Deployment_script.ps1
@@ -4,5 +4,5 @@ Select-AzSubscription -SubscriptionName "Azure subscription 1"
 $mainTemplateUri="https://raw.githubusercontent.com/oboiko7/appserviceplan/main/main.json"
 New-AzResourceGroupDeployment `
   -Name Homework `
-  -ResourceGroupName TestRG1 `
+  -ResourceGroupName RGAppService `
   -TemplateUri $mainTemplateUri
